eb156f2b05cd5a1a4288635526e48113826bf637
Nicolas Ledez <github@ledez.net>
2010-12-14 23:50:07 +0100

    Version 1.0

---
+++ users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all.paginate
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @users }
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @user }
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(users_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
